0347e2f4caa0a3470bc401b4069afea1960e4fc4
Catherine Bronzo <cbronzo@mac.com>
2018-08-06 20:35:38 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -4,13 +4,15 @@ class App < Sinatra::Base
   
   
   get '/name' do
-    "My name is "
+    "My name is Catherine."
   end
 
   get '/hometown' do
+    "My hometown is Hampton."
   end
 
   get '/favorite-song' do
+    "My favorite song is Lose It"
   end
 
 end
